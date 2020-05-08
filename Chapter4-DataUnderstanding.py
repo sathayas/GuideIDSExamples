a3d87a72fff5bf8daf1562b6969b6c725c8fb0f8
Satoru Hayasaka <satoru.hayasaka@knime.com>
2020-05-08 11:40:46 -0500

    the data is in a data frame now, instead of a numpy array

--- Chapter4-DataUnderstanding.py
+++ Chapter4-DataUnderstanding.py
@@ -1,16 +1,19 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets
 
 # loading the iris data set
 iris = datasets.load_iris()
-X = iris.data   # 2D data array
-varNames = iris.feature_names  # variable names
+X = pd.DataFrame(iris.data)   # 2D data array
+X.columns = ['sepal length', 'sepal width', 'petal length', 'petal width']
 
 # histogram of the petal length
-plt.hist(X[:,2])
+plt.hist(X['petal length'])
 plt.show()
 
 # histogram with 20 bins
-plt.hist(X[:,2], 20)
+plt.hist(X['petal length'], 20)
 plt.show()
+
+# box plot of the petal length
