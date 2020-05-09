0e1bf34122994554c4c476e1b1e8cf0d5a3de449
Satoru Hayasaka <satoru.hayasaka@knime.com>
2020-05-09 16:54:51 -0500

    added column name to data frame y; scatter plot matrix with different colors

--- Chapter4-DataUnderstanding.py
+++ Chapter4-DataUnderstanding.py
@@ -5,9 +5,11 @@ from sklearn import datasets
 
 # loading the iris data set
 iris = datasets.load_iris()
-X = pd.DataFrame(iris.data)   # 2D data array
+X = pd.DataFrame(iris.data)   # features data frame
 X.columns = ['sepal length', 'sepal width', 'petal length', 'petal width']
-
+y = pd.DataFrame(iris.target)  # target data frame
+y.columns = ['species']
+target_names = iris.target_names
 
 #
 # HISTOGRAMS
@@ -50,6 +52,18 @@ print(X.describe())
 # SCATTER PLOTS
 #
 
-# plotting petal width vs length
+# plotting petal width vs length (as a method)
 X.plot.scatter(x='petal width', y='petal length')
+plt.show()
+
+# plotting petal width vs length (as a function)
+plt.scatter(X['petal width'], X['petal length'])
+plt.show()
+
+# scatter plot matrix
+pd.plotting.scatter_matrix(X)
+plt.show()
+
+# scatter plot matrix with different colors for species
+pd.plotting.scatter_matrix(X, c=y['species'])
 plt.show()
