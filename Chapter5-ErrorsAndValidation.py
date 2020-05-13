3454037240c6727003015417ef94cc6dd42ad2f1
Satoru Hayasaka <satoru.hayasaka@knime.com>
2020-05-13 12:32:24 -0500

    Now imports classifier evaluation functions; Naive Bayes classifier and performance evaluation have been added

--- Chapter5-ErrorsAndValidation.py
+++ Chapter5-ErrorsAndValidation.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-
+from sklearn.metrics import confusion_matrix
 
 # Loading the iris data
 iris = datasets.load_iris()
@@ -14,3 +14,14 @@ target_names = iris.target_names   # target names
 # spliting the data into training and testing data sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.333,
                                                     random_state=2020)
+
+# Gaussian naive Bayes classifier
+gnb = GaussianNB()  # defining the classifier object
+gnb.fit(X_train, y_train)  # training the classifier with the training set
+y_pred = gnb.predict(X_test)  # generating prediction with trained classifier
+
+# confusion matrix
+print(confusion_matrix(y_test,y_pred))
+
+# classification report
+print(classification_report(y_test, y_pred, target_names=target_names))
