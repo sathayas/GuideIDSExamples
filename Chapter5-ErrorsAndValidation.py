64dd486103cb63b6f6c8ea0262f607d48fae5192
Satoru Hayasaka <satoru.hayasaka@knime.com>
2020-05-13 11:04:41 -0500

    Corrected the library name for the datasets in sklearn

--- Chapter5-ErrorsAndValidation.py
+++ Chapter5-ErrorsAndValidation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.datasets import load_iris
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
