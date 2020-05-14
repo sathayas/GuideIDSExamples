8e0f5dfb407b66825bd3d3cbd539c83cd07f9b9e
Satoru Hayasaka <satoru.hayasaka@knime.com>
2020-05-14 16:48:13 -0500

    Debugged the by creating data frames for the imputation example

--- Chapter6-DataPreparation.py
+++ Chapter6-DataPreparation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.impute import SimpleImputer
 
 #
@@ -6,12 +7,16 @@ from sklearn.impute import SimpleImputer
 #
 
 # Creating toy data sets (numerical)
-X_train = np.array([3, 2, 1, 4, 5, np.nan, np.nan, 5]).reshape(-1,1)
-X_test = np.array([3, 2, np.nan, 4, np.nan, np.nan, 1, np.nan]).reshape(-1,1)
+X_train = pd.DataFrame()
+X_train['X'] = [3, 2, 1, 4, 5, np.nan, np.nan, 5]
+X_test = pd.DataFrame()
+X_test['X'] = [3, 2, np.nan, 4, np.nan, np.nan, 1, np.nan]
 
 # Creating toy data sets (categorical)
-S_train = np.array(['Hi', 'Med', 'Med', 'Hi', 'Low', 'Med', '', 'Med']).reshape(-1,1)
-S_test = np.array(['Hi', '', '', 'Hi', 'Med', '', 'Low']).reshape(-1,1)
+S_train = pd.DataFrame()
+S_train['S'] = ['Hi', 'Med', 'Med', 'Hi', 'Low', 'Med', np.nan, 'Med']
+S_test = pd.DataFrame()
+S_test['S'] = ['Hi', np.nan, np.nan, 'Hi', 'Med', np.nan, 'Low']
 
 # Imputing numerical data with mean
 imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -19,6 +24,6 @@ X_train_imp = imp_mean.fit_transform(X_train)
 X_test_imp = imp_mean.transform(X_test)
 
 # Imputing categorical data with mode
-imp_mode = SimpleImputer(missing_values='', strategy='most_frequent')
+imp_mode = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
 S_train_imp = imp_mode.fit_transform(S_train)
 S_test_imp = imp_mode.transform(S_test)
